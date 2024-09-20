a34a427d38a34346d2d92fda4a1706abefe30a5e
kapilpandey10 <kapilpandey2068@gmail.com>
2024-09-20 17:04:06 +1000

    updated news & Media

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -14,6 +14,11 @@ const useQuery = () => {
   return new URLSearchParams(useLocation().search);
 };
 
+// Function to generate URL slugs for title
+const generateSlug = (title) => {
+  return title.trim().replace(/\s+/g, '_').toLowerCase(); // Replaces spaces with underscores and converts to lowercase
+};
+
 const SearchResults = () => {
   const [results, setResults] = useState([]);
   const query = useQuery().get('query');
@@ -23,7 +28,7 @@ const SearchResults = () => {
       const { data, error } = await supabase
         .from('lyrics')
         .select('*')
-        .or(`title.ilike.%${query}%,lyrics.ilike.%${query}%,artist.ilike.%${query}%`); // Combined or condition
+        .or(`title.ilike.%${query}%,lyrics.ilike.%${query}%,artist.ilike.%${query}%`); // Combined OR condition
 
       if (error) {
         console.error('Error fetching search results:', error);
@@ -46,18 +51,16 @@ const SearchResults = () => {
             <li key={lyric.id}>
               <h2>{lyric.title}</h2>
               <p>{lyric.artist}</p>
-              <p><a href={`/lyrics/${lyric.id}`}>View Lyrics</a></p>
+              {/* Use the slug generation function to create dynamic URL */}
+              <p><a href={`/lyrics/${generateSlug(lyric.title)}`}>View Lyrics</a></p>
             </li>
           ))}
         </ul>
       ) : (
-        <p>No results found for "{query}".Please type different Keywords.</p>
+        <p>No results found for "{query}". Please type different keywords.</p>
       )}
     </div>
   );
 };
 
 export default SearchResults;
-
-
-

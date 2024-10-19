a7586f0c75638bb0b17233f7d028a533ceb033e7
Kapil Pandey <kapilpandey2068@gmail.com>
2024-10-19 19:36:12 +1100

    third attack

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import AdminLogin from './admin/AdminLogin'; // Adjust the path based on your file structure
+import AdminLogin from './Admin/AdminLogin'; // Adjust the path based on your file structure
 
 const Login = () => {
   return <AdminLogin />;

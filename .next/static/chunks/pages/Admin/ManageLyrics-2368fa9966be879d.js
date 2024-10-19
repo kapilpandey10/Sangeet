57530b6a0445e66a6af31daf9721788ff0283ef5
Kapil Pandey <kapilpandey2068@gmail.com>
2024-10-19 19:13:33 +1100

    Delete .next directory

--- .next/static/chunks/pages/Admin/ManageLyrics-2368fa9966be879d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[369],{8621:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Admin/ManageLyrics",function(){return u(466)}])}},function(n){n.O(0,[372,466,888,774,179],function(){return n(n.s=8621)}),_N_E=n.O()}]);
